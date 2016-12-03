b367553beb4c5e1632090c9c6889406ff99ba586
Nabeel Shaheen <nabsha.code@gmail.com>
2016-12-03 18:44:34 +1100

    refactoring and error handling

--- server.js
+++ server.js
@@ -1,26 +1,58 @@
-var express    = require('express'),
-    fileUpload = require('express-fileupload'),
-    app        = express(),
-    fs = require('fs')
-    unzip = require('unzip'),
-        glob = require('glob');
+
+var express = require('express');
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
+var unzip = require('unzip');
+var glob = require('glob');
 
 var raml = require('raml-parser');
 var toRAML = require('raml-object-to-raml');
 var rmdir = require('rmdir');
 
-app.use('/form', express.static(__dirname + '/upload.test.html'));
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// default options
+
 app.use(fileUpload());
 
-app.get('/ping', function(req, res) {
+app.get('/ping', function (req, res) {
     res.send('pong');
 });
 
-app.post('/upload', function(req, res) {
+function uberGenerator(file, res, uploadPath, unzipDirectory) {
+    raml.loadFile(file).then(function (data) {
+        res.contentType('application/json').send(toRAML(data))
+        fs.unlink(uploadPath, function (err) {
+            if (err) throw err;
+            console.log('successfully deleted ' + uploadPath);
+        });
+
+        rmdir(unzipDirectory, function (err, dirs, files) {
+            console.log('all files are removed');
+        });
+    }, function (error) {
+        console.log('Error parsing: ' + error);
+    });
+}
+
+function successfulUnzip(unzipDirectory, res, uploadPath) {
+    console.log("Extraction complete " + unzipDirectory)
+    glob(unzipDirectory + "/*.raml", function (err, files) {
+        console.log(files)
+        if (!err) {
+            files.forEach(function (file) {
+                    uberGenerator(file, res, uploadPath, unzipDirectory);
+                }
+            )
+        } else {
+            res.status(400).send("No raml file found in the zipped file" + err)
+        }
+    })
+}
+
+
+app.post('/upload', function (req, res) {
     var sampleFile, uploadPath;
 
     if (!req.files) {
@@ -32,40 +64,27 @@ app.post('/upload', function(req, res) {
 
     uploadPath = '/tmp/' + sampleFile.name + new Date().getTime();
 
-    sampleFile.mv(uploadPath, function(err) {
+    sampleFile.mv(uploadPath, function (err) {
         if (err) {
             res.status(500).send(err);
         }
     });
 
     // extract files
-    var unzipDirectory = uploadPath + ".d";
-    var unzipExtractor = unzip.Extract({ path: unzipDirectory });
-    unzipExtractor.on('close', function() {
-        console.log("Extraction complete " + unzipDirectory)
-            glob(unzipDirectory + "/*.raml", function (er, files) {
-                console.log(files)
-                if (!er) {
-                    files.forEach( function(file) {
-                        raml.loadFile(file).then( function(data) {
-                            res.contentType('application/json').send(toRAML(data))
-                            fs.unlink(uploadPath, function (err) {
-                                if (err) throw err;
-                            console.log('successfully deleted ' + uploadPath);
-                        });
 
-                            rmdir(unzipDirectory, function (err, dirs, files) {
-                                console.log(dirs);
-                                console.log(files);
-                                console.log('all files are removed');
-                            });
-                        }, function(error) {
-                            console.log('Error parsing: ' + error);
-                        });
-                    }
-                    )
-                }
-            })
+    var unzipDirectory = uploadPath + ".d";
+    var unzipExtractor = unzip.Extract({
+        path: unzipDirectory
+    });
+
+    unzipExtractor.on('error', function (err) {
+        if (err) {
+            res.status(400).send("Failed to unzip file, bad input file" + err)
+        }
+    });
+
+    unzipExtractor.on('close', function () {
+        successfulUnzip(unzipDirectory, res, uploadPath);
     });
 
     fs.createReadStream(uploadPath).pipe(unzipExtractor);
@@ -73,6 +92,6 @@ app.post('/upload', function(req, res) {
 
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 })
